59fba39819d90b0f7698849c2dc248c5e66a7a68
Sana Desai <sana.desai.inventyv@gmail.com>
2026-01-25 22:22:14 +0530

    cleanup build files

--- _2_structures/target/debug/_2_structures.d
+++
@@ -1,1 +0,0 @@
-/home/sana_d/rust_learning/_2_structures/target/debug/_2_structures: /home/sana_d/rust_learning/_2_structures/src/main.rs
--- _2_structures/target/debug/deps/_2_structures-eaf682708ea64590.d
+++
@@ -1,5 +0,0 @@
-/home/sana_d/rust_learning/_2_structures/target/debug/deps/_2_structures-eaf682708ea64590.d: src/main.rs
-
-/home/sana_d/rust_learning/_2_structures/target/debug/deps/_2_structures-eaf682708ea64590: src/main.rs
-
-src/main.rs:
